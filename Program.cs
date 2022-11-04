4daeb3e5d8b72e86575c065e8287d20caa6b2fe3
Beatrix Devanti <beatrix.devanti.ft21@mail.umy.ac.id>
2022-11-04 15:49:27 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerchise2_Algoritma
+{
+    internal class Program
+    {
+        int[] beatrix = new int[21];
+
+        //Number of elements in array
+        int n, i, ba, val, flag;
+        void input()
+        {
+
+            while (true)
+            {
+                Console.Write("Masukkan jumlah element array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 21)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat memiliki maksimum 21 element \n");
+
+            }
+        }
+    }
+}
+ 
+
+
